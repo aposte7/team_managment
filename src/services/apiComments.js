0bf06ae5f42be4eca14423210a674faf21c7f7b8
aposte7 <olman4jesus@gmail.com>
2025-07-08 23:08:17 +0300

    feat: implement createComment function to handle comment and todo creation with error handling

--- src/services/apiComments.js
+++ src/services/apiComments.js
@@ -12,3 +12,39 @@ export async function getComment(memberId) {
 
   return data;
 }
+
+export async function createComment(newCommentData, memberId) {
+  const { comment, todos = [], tags = [] } = newCommentData;
+
+  const { error: commentError, data: commentData } = await supabaseClient
+    .from("comments")
+    .insert([{ ...comment, member_id: memberId }])
+    .select()
+    .single();
+
+  if (commentError) {
+    throw new Error("Comment could not be created");
+  }
+
+  const commentId = commentData.id;
+
+  const todosWithCommentId = todos.map((todo) => ({
+    ...todo,
+    comment_id: commentId,
+    is_done: false,
+  }));
+
+  const { error: todosError, data: todosData } = await supabaseClient
+    .from("todos")
+    .insert(todosWithCommentId)
+    .select();
+
+  if (todosError) {
+    throw new Error("Todos could not be created");
+  }
+
+  return {
+    comment: commentData,
+    todos: todosData,
+  };
+}
