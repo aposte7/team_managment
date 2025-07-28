e0f1ca2d101b9f0ed0c6e3908ab7042ac19f4302
aposte7 <olman4jesus@gmail.com>
2025-07-28 08:21:48 +0300

    feat: implement createAttendance function and integrate it into createSession for attendance management

--- src/services/apiAttendance.js
+++ src/services/apiAttendance.js
@@ -10,3 +10,25 @@ export async function getAttendance() {
   }
   return data;
 }
+
+export async function createAttendance(session) {
+  const { data: members, error: memberError } = await supabaseClient
+    .from("members")
+    .select("id");
+
+  if (memberError) throw memberError;
+
+  const attendances = members.map((member) => ({
+    session_id: session.id,
+    member_id: member.id,
+    status: session.default_status,
+  }));
+
+  const { data, error: attendanceError } = await supabaseClient
+    .from("attendance")
+    .insert(attendances);
+
+  if (attendanceError) throw attendanceError;
+
+  return data;
+}
--- src/services/apiSession.js
+++ src/services/apiSession.js
@@ -1,14 +1,20 @@
+import { createAttendance } from "./apiAttendance";
 import supabaseClient from "./supabase";
 
 export async function createSession(newData) {
   const { data, error } = await supabaseClient
     .from("session")
-    .insert([newData]);
+    .insert([newData])
+    .select()
+    .single();
 
   if (error) {
     console.error("Supabase insert error:", error);
     throw new Error(error);
   }
+  await createAttendance(data);
+
+  return data;
 }
 
 export async function getSession() {
